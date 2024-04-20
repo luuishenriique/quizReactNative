8716dc4c19eeb4d3892648645f5bee97c3044ba6
luuishenriique <luis_henrique_co@hotmail.com>
2024-04-20 10:47:08 -0300

    Adding navigation modulation and dependencies

--- App.jsx
+++ App.jsx
@@ -1,23 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack' 
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'  
 import ModelScreen from './screens/ModelScreen';
 import HomeScreen from './screens/HomeScreen';
 import AboutScreen from './screens/AboutScreen';
-import GeneralQuizScreen from './screens/GeneralQuizScreen';
-import DesenhosScreen from './screens/DesenhosScreen';
 
 const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+const Tabs = createBottomTabNavigator()
 
 export default function App() {
+  let Navigation = Drawer; 
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Model' component={ModelScreen} />
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='Sobre' component={AboutScreen} />
-      </Stack.Navigator>
+     {<Navigation.Navigator initialRouteName='Home'>
+        <Navigation.Screen name='Model' component={ModelScreen} />
+        <Navigation.Screen name='Home' component={HomeScreen} />
+        <Navigation.Screen name='Sobre' component={AboutScreen} />
+      </Navigation.Navigator>}
     </NavigationContainer>
   );
 }
